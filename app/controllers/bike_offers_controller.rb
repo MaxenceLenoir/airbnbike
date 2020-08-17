6786b5cf430ab3d583cd100271c587c659e935df
Marie Lepers <mv.lepers@gmail.com>
2020-08-17 15:58:27 +0200

    user can create a bike and list all bikes

--- app/controllers/bike_offers_controller.rb
+++ app/controllers/bike_offers_controller.rb
@@ -1,5 +1,32 @@
 class BikeOffersController < ApplicationController
+    
+    def index
+        @bike_offers = BikeOffer.all
+    end
+    
+    
     def new
         @bike_offer = BikeOffer.new
     end
+
+    def create
+        @bike_offer = BikeOffer.new(bike_offer_params)
+
+        if @bike_offer.save
+            redirect_to bike_offers_path
+        else
+            render :new
+        end
+
+    end
+
+
+
+    private
+
+    def bike_offer_params
+        params.require(:bike_offer).permit(:title, :price_per_day, :size, :genre, :user)
+    end
+
 end
+
--- app/models/bike_offer.rb
+++ app/models/bike_offer.rb
@@ -1,2 +1,3 @@
 class BikeOffer < ApplicationRecord
+    validates :size, :price_per_day, :genre, :title, presence: true
 end
