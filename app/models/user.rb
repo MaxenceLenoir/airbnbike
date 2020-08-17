fea6da802124d218140f5beaa56381b741be9924
Marie Lepers <mv.lepers@gmail.com>
2020-08-17 17:11:51 +0200

    added link between user and bike

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,5 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   validates :pseudo, presence: true
+  has_many :bike_offers
 end
