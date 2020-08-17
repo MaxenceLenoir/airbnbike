fe67b42a6de13bbc678a6637397d73f291ff8cc0
Marie Lepers <mv.lepers@gmail.com>
2020-08-17 17:22:41 +0200

    add a user for each bike under creation

--- app/controllers/bike_offers_controller.rb
+++ app/controllers/bike_offers_controller.rb
@@ -15,7 +15,8 @@ class BikeOffersController < ApplicationController
 
     def create
         @bike_offer = BikeOffer.new(bike_offer_params)
-
+        @bike_offer.user = current_user
+        
         if @bike_offer.save
             redirect_to @bike_offer
         else
@@ -32,7 +33,7 @@ class BikeOffersController < ApplicationController
     private
 
     def bike_offer_params
-        params.require(:bike_offer).permit(:title, :price_per_day, :size, :genre, :user)
+        params.require(:bike_offer).permit(:title, :price_per_day, :size, :genre, :user_id)
     end
 
     def set_bike_offer
