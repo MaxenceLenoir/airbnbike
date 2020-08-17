0740449b6563c3dd000b8b83a8c726bac4126a20
Marie Lepers <mv.lepers@gmail.com>
2020-08-17 16:22:25 +0200

    a user can see details on a bike

--- app/controllers/bike_offers_controller.rb
+++ app/controllers/bike_offers_controller.rb
@@ -1,5 +1,9 @@
 class BikeOffersController < ApplicationController
     
+    before_action :set_bike_offer, only: [:show]
+
+
+
     def index
         @bike_offers = BikeOffer.all
     end
@@ -13,11 +17,14 @@ class BikeOffersController < ApplicationController
         @bike_offer = BikeOffer.new(bike_offer_params)
 
         if @bike_offer.save
-            redirect_to bike_offers_path
+            redirect_to @bike_offer
         else
             render :new
         end
 
+    end
+
+    def show
     end
 
 
@@ -26,6 +33,10 @@ class BikeOffersController < ApplicationController
 
     def bike_offer_params
         params.require(:bike_offer).permit(:title, :price_per_day, :size, :genre, :user)
+    end
+
+    def set_bike_offer
+        @bike_offer = BikeOffer.find(params[:id])
     end
 
 end
