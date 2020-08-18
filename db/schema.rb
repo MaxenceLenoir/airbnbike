c01f9a0ba6ff934292bf8de63ee349147b2bca7d
Thomas Quetu <quetu.thomas@gmail.com>
2020-08-18 11:05:23 +0200

    homepage2

--- db/schema.rb
+++ db/schema.rb
@@ -23,6 +23,7 @@ ActiveRecord::Schema.define(version: 2020_08_17_132754) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.string "title"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
